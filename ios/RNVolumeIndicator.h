767248ce6001655fa11a4ad95db94be6c8956f43
Maxime Nory <maximenory@yahoo.fr>
2018-07-01 22:22:01 +0200

    fix imports

--- ios/RNVolumeIndicator.h
+++ ios/RNVolumeIndicator.h
@@ -1,9 +1,8 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
+#if __has_include(<React/RCTBridgeModule.h>)
+  #import <React/RCTBridgeModule.h>
+  #import <React/RCTEventEmitter.h>
 #else
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
+  #import "RCTBridgeModule.h"
 #endif
 
 #import <AVFoundation/AVAudioSession.h>
